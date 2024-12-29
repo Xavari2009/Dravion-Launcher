4fed6ebf40b9f1ea5456fdc16d0d3e3d2889c9f6
Xavari2009 <Xavieurman2@gmail.com>
2024-12-28 19:19:58 -0500

    Add files via upload

---
+++ .github/Dravion-Launcher-WEB-Folder/files/php/instances.php
@@ -0,0 +1,28 @@
+<?php
+$instance['MainV1'] = array_merge($instance['MainV1'], array(
+    "loadder" => array(
+        "minecraft_version" => "1.8.9",
+        "loadder_type" => "forge",
+        "loadder_version" => "latest"
+    ),
+    "verify" => true,
+    "ignored" => array(
+        'config',
+        'essential',
+        'logs',
+        'resourcepacks',
+        'saves',
+        'screenshots',
+        'shaderpacks',
+        'W-OVERFLOW',
+        'options.txt',
+        'optionsof.txt'
+    ),
+    "whitelist" => array(),
+    "whitelistActive" => false,
+    "status" => array(
+        "nameServer" => "Hypixel",
+        "ip" => "mc.hypixel.net",
+        "port" => 25565
+    )
+));
